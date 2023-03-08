b7e25c3efcfe0056c56eb26509f22e5ac6e22301
寂笙 <1971649453@qq.com>
2023-03-08 14:52:11 +0800

    day12 项目持续部署

--- heima-leadnews-services/admin-service/src/main/java/com/heima/admin/controller/v1/ChannelController.java
+++ heima-leadnews-services/admin-service/src/main/java/com/heima/admin/controller/v1/ChannelController.java
@@ -33,7 +33,7 @@ public class ChannelController {
     public ResponseResult findAll() {
         List<AdChannel> list = channelService.list();
         ResponseResult result = ResponseResult.okResult(list);
-        result.setErrorMessage("测试自动部署1");
+        result.setErrorMessage("测试自动部署2");
         return result;
     }
 
